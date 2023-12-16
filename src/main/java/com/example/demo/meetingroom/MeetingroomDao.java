9c2d709a880e1578a77c1c1e6470ddc1d9f6088e
김현주 <hyeonju926@gmail.com>
2023-12-16 22:27:51 +0900

    meetingroom 이벤트 중복
    meetingroom 이벤트 중복 안되게

--- src/main/java/com/example/demo/meetingroom/MeetingroomDao.java
+++ src/main/java/com/example/demo/meetingroom/MeetingroomDao.java
@@ -2,8 +2,13 @@ package com.example.demo.meetingroom;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MeetingroomDao extends JpaRepository<Meetingroom, Integer> {
     List<Meetingroom> findByRoomId(int roomId);
+
+    List<Meetingroom> findByRoomIdAndStartDateLessThanEqualAndEndDateGreaterThanEqual(
+            int roomId, LocalDateTime endDate, LocalDateTime startDate);
 }
