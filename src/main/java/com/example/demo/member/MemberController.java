1df0e3a26da730ab34fd61381a34272d42df65f4
Cha <tjddnr9553@naver.com>
2023-12-21 15:37:43 +0900

    로그인시 세션저장소에 마스터유무 확인 추가
    로그인시 세션저장소에 마스터유무 확인 추가

--- src/main/java/com/example/demo/member/MemberController.java
+++ src/main/java/com/example/demo/member/MemberController.java
@@ -44,225 +44,226 @@ import java.util.Map;
 @CrossOrigin(origins = "*") // 모든 ip로부터 요청 받기 허용
 public class MemberController {
 
-	private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
-	@Autowired
-	private MemberService service;
+    @Autowired
+    private MemberService service;
 
-	@Autowired
-	private JwtTokenProvider tokenprovider;
+    @Autowired
+    private JwtTokenProvider tokenprovider;
 
-	@Autowired
-	private AuthenticationManagerBuilder authenticationManagerBuilder;
+    @Autowired
+    private AuthenticationManagerBuilder authenticationManagerBuilder;
 
-	@Value("${spring.servlet.multipart.location}")
-	private String path;
+    @Value("${spring.servlet.multipart.location}")
+    private String path;
 
-	@PostMapping("/login")
-	public Map login(String username, String password) {
-		System.out.println(username + " , " + password);
-		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
-				password);
-		System.out.println(authenticationToken);
-		Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-		System.out.println(authentication.isAuthenticated());
+    @PostMapping("/login")
+    public Map login(String username, String password) {
+        System.out.println(username + " , " + password);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+                password);
+        System.out.println(authenticationToken);
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        System.out.println(authentication.isAuthenticated());
+        MemberDto dto = service.getMember(username);
+        Map map = new HashMap();
+        boolean flag = authentication.isAuthenticated();
+        System.out.println(flag);
+        if (flag) {
+            String token = tokenprovider.generateJwtToken(new MemberDto(null, username, "", "", "", "", "", "", 0, "",
+                    0, "", "", "", 0, 0, 0, "", "", 0, 0, 0, null));
+            flag = true;
+            map.put("token", token);
+            map.put("dto", dto);
+        }
+        map.put("flag", flag);
+        return map;
+    }
 
-		Map map = new HashMap();
-		boolean flag = authentication.isAuthenticated();
-		System.out.println(flag);
-		if (flag) {
-			String token = tokenprovider.generateJwtToken(new MemberDto(null, username, "", "", "", "", "", "", 0, "",
-					0, "", "", "", 0, 0, 0, "", "", 0, 0, 0, null));
-			flag = true;
-			map.put("token", token);
-		}
-		map.put("flag", flag);
-		return map;
-	}
+    @GetMapping("/auth/mypage")
+    public Map mypage() {
+        Map map = new HashMap();
+        boolean flag = true;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String id = authentication.getName();// 로그인 아이디 추출
+        MemberDto mdto = service.getMember(id);
+        System.out.println(mdto);
+        if (mdto == null) {
+            flag = false;
+        } else {
+            map.put("mdto", mdto);
+        }
+        map.put("flag", flag);
+        return map;
+    }
 
-	@GetMapping("/auth/mypage")
-	public Map mypage() {
-		Map map = new HashMap();
-		boolean flag = true;
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String id = authentication.getName();// 로그인 아이디 추출
-		MemberDto mdto = service.getMember(id);
-		System.out.println(mdto);
-		if (mdto == null) {
-			flag = false;
-		} else {
-			map.put("mdto", mdto);
-		}
-		map.put("flag", flag);
-		return map;
-	}
+    @PostMapping("/join")
+    public Map join(@RequestBody MemberJoinDto memberJoinDto) {
+        MemberDto res = service.create(memberJoinDto);
+        Map map = new HashMap();
+        map.put("name", res.getName());
+        return map;
+    }
 
-	@PostMapping("/join")
-	public Map join(@RequestBody MemberJoinDto memberJoinDto) {
-		MemberDto res = service.create(memberJoinDto);
-		Map map = new HashMap();
-		map.put("name", res.getName());
-		return map;
-	}
+    @GetMapping("/auth/info")
+    public Map myinfo() {
+        Map map = new HashMap();
+        boolean flag = true;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String id = authentication.getName();// 로그인 아이디 추출
+        MemberDto m = service.getMember(id);
+        System.out.println(m);
+        if (m == null) {
+            flag = false;
+        } else {
+            map.put("m", m);
+        }
+        map.put("flag", flag);
+        return map;
+    }
 
-	@GetMapping("/auth/info")
-	public Map myinfo() {
-		Map map = new HashMap();
-		boolean flag = true;
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String id = authentication.getName();// 로그인 아이디 추출
-		MemberDto m = service.getMember(id);
-		System.out.println(m);
-		if (m == null) {
-			flag = false;
-		} else {
-			map.put("m", m);
-		}
-		map.put("flag", flag);
-		return map;
-	}
+    @GetMapping("/edit/{username}")
+    public Map editForm(String username) {
+        Map map = new HashMap();
+        MemberDto dto = service.getMember(username);
+        map.put("m", dto);
+        return map;
+    }
 
-	@GetMapping("/edit/{username}")
-	public Map editForm(String username) {
-		Map map = new HashMap();
-		MemberDto dto = service.getMember(username);
-		map.put("m", dto);
-		return map;
-	}
+    @PutMapping("/edit")
+    public Map edit(@RequestBody MemberDto dto) {
+        Map map = new HashMap();
+        System.out.println("dto.getDeptCode() = " + dto.getDeptCode());
+        System.out.println("dto.getDeptName() = " + dto.getDeptName());
+        System.out.println("dto.getPwd() = " + dto.getPwd());
 
-	@PutMapping("/edit")
-	public Map edit(@RequestBody MemberDto dto) {
-		Map map = new HashMap();
-		System.out.println("dto.getDeptCode() = " + dto.getDeptCode());
-		System.out.println("dto.getDeptName() = " + dto.getDeptName());
-		System.out.println("dto.getPwd() = " + dto.getPwd());
+        MemberDto m = service.getMemberByName(dto.getName());
+        m.setUsername(dto.getUsername());
+        m.setPwd(dto.getPwd());
+        m.setName(dto.getName());
+        m.setCompanyName(dto.getCompanyName());
+        m.setDeptCode(dto.getDeptCode());
+        m.setCompanyRank(dto.getCompanyRank());
+        m.setNewNo(dto.getNewNo());
+        m.setEmail(dto.getEmail());
+        m.setAddress(dto.getAddress());
+        m.setComCall(dto.getComCall());
+        m.setPhone(dto.getPhone());
+        m.setIsMaster(dto.getIsMaster());
+        m.setStatus(dto.getStatus());
+        service.save(m);
+        ArrayList<MemberDto> list = service.getAll();
+        map.put('m', m);
+        map.put("list", list);
+        return map;
+    }
 
-		MemberDto m = service.getMemberByName(dto.getName());
-		m.setUsername(dto.getUsername());
-		m.setPwd(dto.getPwd());
-		m.setName(dto.getName());
-		m.setCompanyName(dto.getCompanyName());
-		m.setDeptCode(dto.getDeptCode());
-		m.setCompanyRank(dto.getCompanyRank());
-		m.setNewNo(dto.getNewNo());
-		m.setEmail(dto.getEmail());
-		m.setAddress(dto.getAddress());
-		m.setComCall(dto.getComCall());
-		m.setPhone(dto.getPhone());
-		m.setIsMaster(dto.getIsMaster());
-		m.setStatus(dto.getStatus());
-		service.save(m);
-		ArrayList<MemberDto> list = service.getAll();
-		map.put('m', m);
-		map.put("list", list);
-		return map;
-	}
+    @Transactional
+    @PostMapping("/del")
+    public Map delete(String username) {
+        System.out.println("username = " + username);
+        service.delete(username);
+        Map map = new HashMap();
+        ArrayList<MemberDto> list = service.getAll();
+        map.put("list", list);
+        return map;
+    }
 
-	@Transactional
-	@PostMapping("/del")
-	public Map delete(String username) {
-		System.out.println("username = " + username);
-		service.delete(username);
-		Map map = new HashMap();
-		ArrayList<MemberDto> list = service.getAll();
-		map.put("list", list);
-		return map;
-	}
+    @PutMapping("/auth/pwdedit")
+    public Map pwdedit(MemberDto dto) {
 
-	@PutMapping("/auth/pwdedit")
-	public Map pwdedit(MemberDto dto) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String id = authentication.getName();
+        MemberDto m = service.getMember(id);
+        if (dto.getF() != null) {
+            MultipartFile f = dto.getF();
+            String fname = f.getOriginalFilename();
+            File newFile = new File(path + fname);
+            try {
+                f.transferTo(newFile);
+                m.setOriginFname(fname);
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("test" + dto.getF());
 
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String id = authentication.getName();
-		MemberDto m = service.getMember(id);
-		if (dto.getF() != null) {
-			MultipartFile f = dto.getF();
-			String fname = f.getOriginalFilename();
-			File newFile = new File(path + fname);
-			try {
-				f.transferTo(newFile);
-				m.setOriginFname(fname);
-			} catch (IllegalStateException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		System.out.println("test" + dto.getF());
+        m.setUsername(dto.getUsername());
+        m.setPwd(passwordEncoder.encode(dto.getPwd()));
+        m.setName(dto.getName());
+        m.setCompanyName(dto.getCompanyName());
+        m.setDeptCode(dto.getDeptCode());
+        m.setCompanyRank(dto.getCompanyRank());
+        m.setNewNo(dto.getNewNo());
+        m.setEmail(dto.getEmail());
+        m.setAddress(dto.getAddress());
+        m.setComCall(dto.getComCall());
+        m.setPhone(dto.getPhone());
+        m.setIsMaster(dto.getIsMaster());
+        m.setStatus(dto.getStatus());
+        service.save(m);
+        Map map = new HashMap();
+        map.put("mdto", m);
+        return map;
+    }
 
-		m.setUsername(dto.getUsername());
-		m.setPwd(passwordEncoder.encode(dto.getPwd()));
-		m.setName(dto.getName());
-		m.setCompanyName(dto.getCompanyName());
-		m.setDeptCode(dto.getDeptCode());
-		m.setCompanyRank(dto.getCompanyRank());
-		m.setNewNo(dto.getNewNo());
-		m.setEmail(dto.getEmail());
-		m.setAddress(dto.getAddress());
-		m.setComCall(dto.getComCall());
-		m.setPhone(dto.getPhone());
-		m.setIsMaster(dto.getIsMaster());
-		m.setStatus(dto.getStatus());
-		service.save(m);
-		Map map = new HashMap();
-		map.put("mdto", m);
-		return map;
-	}
+    @GetMapping("/image/{imageName}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String imageName) throws IOException {
 
-	@GetMapping("/image/{imageName}")
-	public ResponseEntity<byte[]> getImage(@PathVariable String imageName) throws IOException {
+        Path imagePath = Paths.get(path, imageName);
+        byte[] imageBytes = Files.readAllBytes(imagePath);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+    }
 
-		Path imagePath = Paths.get(path, imageName);
-		byte[] imageBytes = Files.readAllBytes(imagePath);
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.IMAGE_JPEG);
-		return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
-	}
+    @GetMapping("/monthMemberList")
+    public Map emplist() {
+        ArrayList<MemberDto> list = service.getAll();
+        ArrayList<MemberDto> list2 = new ArrayList<>();
+        for (MemberDto dto : list) {
+            if (dto.getMonthMember() == 1) {
+                list2.add(dto);
+            }
+        }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String id = authentication.getName();
+        MemberDto mdto = service.getMember(id);
+        Map map = new HashMap();
+        map.put("mdto", mdto);
+        map.put("list", list2);
+        return map;
+    }
 
-	@GetMapping("/monthMemberList")
-	public Map emplist() {
-		ArrayList<MemberDto> list = service.getAll();
-		ArrayList<MemberDto> list2 = new ArrayList<>();
-		for (MemberDto dto : list) {
-			if (dto.getMonthMember() == 1) {
-				list2.add(dto);
-			}
-		}
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String id = authentication.getName();
-		MemberDto mdto = service.getMember(id);
-		Map map = new HashMap();
-		map.put("mdto", mdto);
-		map.put("list", list2);
-		return map;
-	}
+    @PutMapping("/monthMember")
+    public Map monthMember(@RequestBody Map<String, List<Long>> requestBody) {
+        List<Long> selectedMemberIds = requestBody.get("selectedMembers");
+        Map map = new HashMap();
+        ArrayList<MemberDto> list = service.getAll();
+        map.put("list", list);
+        for (MemberDto dto : list) {
+            if (selectedMemberIds != null && selectedMemberIds.contains(dto.getId())) {
+                dto.setMonthMember(1);
+            } else {
+                dto.setMonthMember(0);
+            }
+            service.save(dto);
+        }
+        return map;
+    }
 
-	@PutMapping("/monthMember")
-	public Map monthMember(@RequestBody Map<String, List<Long>> requestBody) {
-		List<Long> selectedMemberIds = requestBody.get("selectedMembers");
-		Map map = new HashMap();
-		ArrayList<MemberDto> list = service.getAll();
-		map.put("list", list);
-		for (MemberDto dto : list) {
-			if (selectedMemberIds != null && selectedMemberIds.contains(dto.getId())) {
-				dto.setMonthMember(1);
-			} else {
-				dto.setMonthMember(0);
-			}
-			service.save(dto);
-		}
-		return map;
-	}
+    @GetMapping("/member")
+    public Map getMember() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String id = authentication.getName();
+        MemberDto m = service.getMember(id);
 
-	@GetMapping("/member")
-	public Map getMember() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String id = authentication.getName();
-		MemberDto m = service.getMember(id);
+        Map map = new HashMap();
+        map.put("member", m);
 
-		Map map = new HashMap();
-		map.put("member", m);
-
-		return map;
-	}
+        return map;
+    }
 }
